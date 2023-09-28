4acbc59518f0efe4c7f548900369819a91796051
Jorge Cabiedes <jorgecabiedes@hotmail.com>
2023-09-28 11:31:43 -0600

    Basic echo functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+use std::process;
+
+pub struct Config {
+    pub args: Vec<String>,
+}
+
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let mut output: Vec<String> = Vec::new();
+
+        while let Some(arg) = args.next() {
+            output.push(arg);
+        }
+
+        if output.is_empty() {
+            return Err("No arguments found");
+        }
+
+        return Ok(Config {
+            args: output,
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("{}", config.args.join(" "));
 }
