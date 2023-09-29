3bab2826bc61e037873634ffbf69c0ebeb9c8f7f
Jorge Cabiedes <jorgecabiedes@hotmail.com>
2023-09-28 20:57:01 -0600

    Reformatted into using lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,8 @@
 use std::env;
+use cli_utils_rs::run_echo;
 
 fn main() {
     let output = run_echo(env::args());
 
     println!("{}", output.join(" "));
-}
-
-pub fn run_echo(mut args: impl Iterator<Item = String>) -> Vec<String> {
-    args.next();
-    let mut output: Vec<String> = Vec::new();
-
-    while let Some(arg) = args.next() {
-        output.push(arg);
-    }
-
-    return output;
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn echo_test() {
-        let args = vec!["nil", "arg0"];
-        let result = run_echo(args.into_iter().map(|elm| String::from(elm)));
-
-        assert_eq!(
-            result,
-            vec!["arg0"]
-                .into_iter()
-                .map(|elm| String::from(elm))
-                .collect::<Vec<String>>()
-        );
-
-        let args = vec!["nil", "arg0", "arg1", "arg2"];
-        let result = run_echo(args.into_iter().map(|elm| String::from(elm)));
-
-        assert_eq!(
-            result,
-            vec!["arg0", "arg1", "arg2"]
-                .into_iter()
-                .map(|elm| String::from(elm))
-                .collect::<Vec<String>>()
-        );
-    }
 }
