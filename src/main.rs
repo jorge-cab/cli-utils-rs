f23ccf05ed586870c178239f0786fd3d4399f8d0
Jorge Cabiedes <jorgecabiedes@hotmail.com>
2023-09-28 19:13:31 -0600

    Simplified logic for echo

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,16 @@
 use std::env;
-use std::process;
-
-pub struct Config {
-    pub args: Vec<String>,
-}
-
-impl Config {
-    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        args.next();
-        let mut output: Vec<String> = Vec::new();
-
-        while let Some(arg) = args.next() {
-            output.push(arg);
-        }
-
-        if output.is_empty() {
-            return Err("No arguments found");
-        }
-
-        return Ok(Config {
-            args: output,
-        })
-    }
-}
 
 fn main() {
-    let config = Config::build(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+    echo_function(env::args());
+}
 
-    println!("{}", config.args.join(" "));
+fn echo_function(mut args: impl Iterator<Item = String>) {
+    args.next();
+    let mut output: Vec<String> = Vec::new();
+
+    while let Some(arg) = args.next() {
+        output.push(arg);
+    }
+
+    println!("{}", output.join(" "));
 }
