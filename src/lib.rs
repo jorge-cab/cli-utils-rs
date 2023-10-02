e5a80a9e4718f5a0f3935926ff41723134f3792f
Jorge Cabiedes <jorgecabiedes@hotmail.com>
2023-10-01 21:11:59 -0600

    Created customs errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,27 @@
+use std::error::Error;
+use std::fmt;
 use std::fs;
+
+#[derive(Debug)]
+pub enum AppError {
+    MissingFilePath,
+    FileReadError,
+    InvalidCommand,
+    NoArgumentsReceived,
+}
+
+impl fmt::Display for AppError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            AppError::MissingFilePath => write!(f, "Didn't get a filepath"),
+            AppError::FileReadError => write!(f, "File does not exist"),
+            AppError::InvalidCommand => write!(f, "Invalid Command"),
+            AppError::NoArgumentsReceived => write!(f, "No arguments received"),
+        }
+    }
+}
+
+impl Error for AppError {}
 
 pub fn run_echo(mut args: impl Iterator<Item = String>) -> Vec<String> {
     let mut output: Vec<String> = Vec::new();
@@ -10,31 +33,30 @@ pub fn run_echo(mut args: impl Iterator<Item = String>) -> Vec<String> {
     return output;
 }
 
-pub fn run_cat(mut args: impl Iterator<Item = String>) -> Result<String, &'static str> {
+pub fn run_cat(mut args: impl Iterator<Item = String>) -> Result<String, AppError> {
     let file_path = match args.next() {
         Some(arg) => arg,
-        None => return Err("Didn't get a filepath"),
+        None => return Err(AppError::MissingFilePath),
     };
 
     if let Ok(content) = fs::read_to_string(file_path) {
         return Ok(content);
     } else {
-        return Err("File does not exist.");
+        return Err(AppError::FileReadError);
     };
 }
 
-pub fn bin_selector(mut args: impl Iterator<Item = String>) -> Result<String, &'static str> {
+pub fn bin_selector(mut args: impl Iterator<Item = String>) -> Result<String, AppError> {
     match args.next() {
         Some(arg) if arg == "echo" => Ok(run_echo(args).join(" ")),
         Some(arg) if arg == "cat" => run_cat(args),
-        Some(_) => Err("Invalid Command"),
-        None => Err("No arguments received!"),
+        Some(_) => Err(AppError::InvalidCommand),
+        None => Err(AppError::NoArgumentsReceived),
     }
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::env;
 fn main() {
     let mut args = env::args();
     args.next();
+
     match bin_selector(args) {
         Ok(output) => println!("{}", output),
         Err(err) => eprintln!("{}", err),
